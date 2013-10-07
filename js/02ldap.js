d5aff20b98065b10c7f5ce8dc19fc9f2b20dc50a
smithfarm <presnypreklad@gmail.com>
2013-10-07 18:04:11 +0200

    Struggling with Mason variable scope and JS integration

--- js/02ldap.js
+++ js/02ldap.js
@@ -2,20 +2,13 @@
 
 MFILE.testLDAP = function () {
    $('#mainarea').html(MFILE.html.test_ldap1);
-   MFILE.testLDAPajax();
+   console.log("About to test LDAP");
+   $("#mainarea").append( "<% 'LdapServer' %>" );
    $("#mainarea").append(MFILE.html.press_any_key);
    $("#continue").focus();
    $("#continue").keydown(function(event) {
       event.preventDefault();
       MFILE.state = "MAIN_MENU";
       MFILE.actOnState();
-   });
-}
-
-MFILE.testLDAPajax = function() {
-   console.log("About to test LDAP");
-   $.ajax({
-      url: "testldap",
-      type: "GET",
    });
 }
