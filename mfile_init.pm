6fcd472509675333f4a93e9be6f4ff20a4fcceba
smithfarm <presnypreklad@gmail.com>
2013-10-08 11:26:11 +0200

    Improve initialization

--- mfile_init.pm
+++ mfile_init.pm
@@ -1,4 +1,57 @@
 package mfile_init;
 
-our $IntConfig = {};
-our $ExtConfig = {};
+use DBI;
+use Config::Simple;
+
+# get mfile version number (hardcoded in VERSION file)
+sub _get_mfile_version {
+
+   my $Config = shift;
+
+   if ( open my $fh, "<", $Config->{'RootDir'} . "VERSION" ) {
+      $Global->{'mfilevernum'} = <$fh>;
+      close $fh;
+   } else {
+      $Global->{'mfilevernum'} = $!;
+   }
+
+}
+
+# get MariaDB version number from MariaDB server (and save the connection handle)
+sub _get_mariadb_version {
+
+   my $Config = shift;
+
+   my ( $sql, $sth );
+   my $result = $Global->{'dbh'} = DBI->connect( 'dbi:mysql:' . $Config->{'DbName'}, 
+                                                 $Config->{'DbUsername'}, 
+                                                 $Config->{'DbPassword'} );
+   if ($result) {
+      $sql = "SELECT version();";
+      $sth = $Global->{'dbh'}->prepare($sql);
+   } else {
+      $Global->{'dbvernum'} = "$DBI::errstr\n";
+      return;
+   }
+   if ( $sth->execute ) {
+      $Global->{'dbvernum'} = $sth->fetchrow_array();
+   } else {
+      $Global->{'dbvernum'} = "$DBI::errstr\n";
+   }
+
+}
+
+# load configuration parameters from file
+sub LoadGlobal {
+
+   my $Config = {};
+   my $Global = {};
+
+   # Hopefully, the location of the configuration file is the only
+   # thing we will have hard-coded
+   Config::Simple->import_from('/etc/mfile-mason.conf', $Config);
+   _get_mfile_version($Config);
+   _get_mariadb_version($Config);
+}
+ 
+1;
